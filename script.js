f601b914e28ef64e210d2368068f8566ffc43a00
Alwin-s <alwinsathish260196@gmial.com>
2024-05-08 12:33:36 +0530

    bmi

---
+++ script.js
@@ -0,0 +1,21 @@
+//invalid-weight/height
+//   if(height >300 && weight>200 ){
+//     alert("Enter a valid *Height and *Weight")
+// document.getElementById("move").style.marginLeft="";
+// resultField.value="";
+// const height = document.getElementById("heightas").value="";
+//   const weight = document.getElementById("weightsa").value="";
+
+//   }else if(height >300){
+//     alert("Enter a valid *Height")
+//     document.getElementById("move").style.marginLeft="";
+// resultField.value="";
+// const height = document.getElementById("heightas").value="";
+
+
+//   }else if(weight >200){
+//         alert("Enter a valid *Weight")
+//     document.getElementById("move").style.marginLeft="";
+// resultField.value="";
+// const weight = document.getElementById("weightsa").value="";
+//   }
